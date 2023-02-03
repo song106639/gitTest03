a0ee008371885c07408d0b31e987e40539e21d10
song106639 <slf106639@gmail.com>
2023-02-03 19:29:43 +0800

    第一次提交

---
+++ src/test/java/com/example/gittest03/GitTest03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittest03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
